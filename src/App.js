15e45fb1fc047f3956cf12b6b15ceb51652359ea
tharu <taathu2016@gmail.com>
2019-08-15 20:54:51 -0700

    react refs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.textInput = React.createRef();
+    
+    this.state={
+      firstName:"",
+      lastName:"",
+      age:"",
+      value:""
+    }
+    
+  }
+  onHandleClick = () => {
+    this.setState({
+      value: this.textInput.current.value,  
+    })
+  }
+  onSubmit = () => {
+let fname = 
+    this.setState({
+      firstName: this.firstName.value,
+      lastName: this.lastName.value,
+      age: this.age.value }) 
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>React Ref - textInput</h1>
+        <h3>{this.state.firstName + ' ' + this.state.lastName + ' ' + this.state.age}</h3>
+          <div>
+          <span>First Name: <input type="text" ref={(input) => { this.firstName = input;}}/></span> <br/>
+          <span>Last Name: <input type="text" ref={(input) => { this.lastName = input;}} /></span><br />
+          <span>Age: <input type="text" ref={(input) => { this.age = input; }} /></span><br />
+              <input type="submit" value="Submit" onClick={this.onSubmit}  />
+         </div>
+        <br /><br /><br />
+        <div>
+          <h3>{this.state.value}</h3>
+          
+          <span>Value: <input type="text" ref={this.textInput} /></span><br />
+          <input type="submit" value="Submit" onClick={this.onHandleClick} />
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
